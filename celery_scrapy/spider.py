9c9796304195f28d112ecda6e8abf4ae2db9b7cb
Parual Raheja <xparraj@gmail.com>
2015-12-18 11:17:19 +0530

    Celery

--- celery_scrapy/spider.py
+++ celery_scrapy/spider.py
@@ -1,9 +1,23 @@
 from __future__ import absolute_import
 import scrapy
-from scrapy import Item, Field
+import urllib2
+from bs4 import BeautifulSoup
+from celery_scrapy.items import AmazonItem
 
 
-class AmazonItem(Item):
-    heading = Field()
-    title = Field()
-    link = Field()
+class SpiderAmazon(scrapy.Spider):
+    name = 'amazon'
+
+    allowed_domains = ['amazon.in']
+
+    start_urls = ['http://www.amazon.in/gp/site-directory/ref=nav_shopall_btn']
+
+    def parse(self, response):
+        page = urllib2.urlopen(response.url).read()
+        soup = BeautifulSoup(page)
+
+        list = soup.find_all("div", {"class": "popover-grouping"})
+
+        print "list : ", list
+        print "length of list : ", len(list)
+        yield list
