e3c1634a27657a13618955e761c3dc008f2c1d7b
Parual Raheja <xparraj@gmail.com>
2015-12-24 18:27:21 +0530

    Beautiful Soup

--- beautifulsoup/doctors_details.py
+++ beautifulsoup/doctors_details.py
@@ -14,7 +14,8 @@ soup = BeautifulSoup(data, 'html.parser')
 for div in soup.find_all('div', {'class': 'location-container'}):
     span = div.find('span')
     city = span.string
-    print "CITY : ", re.sub('\s+', '', city)
+    print "\n\n"
+    print "CITY : ", ' '.join(city.split())
 
 
 ######## clinic details
@@ -23,44 +24,44 @@ for div in soup.find_all('div', {'class': 'location-container'}):
 print "\n\t\tDOCTOR DETAILS\n"
 
 for name in soup.find('h1', itemprop="name"):
-    print "name : ", re.sub('\s+', '', name.string)
+    print "Name : \t\t\t\t\t", ' '.join(name.string.split())
 
 for qual in soup.find('p', {'class': 'doctor-qualifications'}):
-    print "qualification : ", re.sub('\s+', '', qual.string)
+    print "\nQualification : \t\t", ' '.join(qual.string.split())
 
 for div in soup.find_all('div', {'class': 'profile-container'}):
     h2 = div.find('h2')
-    spec_exp = re.sub('\s+', '', h2.text)
+    spec_exp = ' '.join(h2.text.split())
     new_text = spec_exp.split(',')
     speciality = new_text[0]
-    print "specialization : ", speciality
+    print "\nSpecialization : \t\t", ' '.join(speciality.split())
     experience = new_text[1]
-    print "experience : ", experience
+    print "\nExperience : \t\t\t", ' '.join(experience.split())
 
 
 div = soup.find('div', {'class': 'doc-info-section organizations-block'})
 exp = div.find('span', {'class': 'exp-tenure'})
-print "\t", re.sub('\s+', '', exp.text)
+print "\t\t\t\t\t\t", ' '.join(exp.text.split())
 exp1 = div.find('span', {'class': 'exp-details'})
-print "\t", re.sub('\s+', '', exp1.text)
+print "\t\t\t\t\t\t", ' '.join(exp1.text.split())
 
-print "Education : "
+print "\nEducation : "
 
 for div in soup.find_all('div', {'class': 'doc-info-section qualifications-block'}):
     for p in div.find_all('p'):
-        print '\t', re.sub('\s+', '', p.text)
+        print "\t\t\t\t\t\t", ' '.join(p.text.split())
 
 
 div = soup.find('div', {'class': 'doc-info-section memberships-block'})
 for p in div.find('p'):
-    print "Memberships : ", re.sub('\s+', '', p)
+    print "\nMemberships : \t\t\t", ' '.join(p.split())
 
-print "Registrations : "
+print "\nRegistrations : "
 div = soup.find('div', {'class': 'doc-info-section registrations-block'})
 reg = div.find('span', {'class': 'exp-tenure'})
-print "\t", re.sub('\s+', '', reg.text)
+print "\t\t\t\t\t\t", ' '.join(reg.text.split())
 reg1 = div.find('span', {'class': 'exp-details'})
-print "\t", re.sub('\s+', '', reg1.text)
+print "\t\t\t\t\t\t", ' '.join(reg1.text.split())
 
 
 timing_days = []
@@ -92,15 +93,15 @@ print "\n\n\t\tCLINIC DETAILS"
 for clinic in soup.find_all('div', 'clinic-address'):
     print "\n"
     for name in clinic.find_all('h2'):
-        print "clinic name : ", re.sub('\s+', '', name.text)
+        print "Clinic Name : \t\t\t", ' '.join(name.text.split())
 
     for address in clinic.find_all('p', itemprop="address"):
-        print "address : ", re.sub('\s+', '', address.text)
+        print "\nAddress : \t\t\t\t", ' '.join(address.text.split())
 
-    print "timings : ", timing_list[i]
-    print "days : ", timing_days[i]
-    print "fees : ", fees_list[i]
-    print "location : ", clinic_location[i]
+    print "\nTimings : \t\t\t\t", timing_list[i]
+    print "\nDays : \t\t\t\t\t", timing_days[i]
+    print "\nFees : \t\t\t\t\t", fees_list[i]
+    print "\nLocation : \t\t\t\t", clinic_location[i]
 
     i = i+1
 
@@ -111,7 +112,7 @@ print "\n\n\t SERVICES\n"
 i = 1
 for div in soup.find_all('div', {'class': 'services-block'}):
     for service in div.find_all('div', {'class': 'service-cell'}):
-        service_name = re.sub('\s+', '', service.text)
-        print i, " : ", service_name
+        service_name = ' '.join(service.text.split())
+        print i, " : \t", service_name
         i = i+1
 
