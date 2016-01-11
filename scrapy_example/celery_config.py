abaa371476147fb21ad963528b13312be56e695d
parul-raheja <parulraheja.01@gmail.com>
2016-01-11 18:27:40 +0530

    scrapy-celery

---
+++ scrapy_example/celery_config.py
@@ -0,0 +1,3 @@
+
+#BROKER_URL = 'mongodb://localhost:27017/celery'
+#CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/celery'
--- scrapy_example/tasks.py
+++ scrapy_example/tasks.py
@@ -5,18 +5,21 @@ from scrapy_example.spiders.new_crawling import MySpider
 from scrapy_example.spiders.crawl_images import ImagesSpider
 from scrapy_example.spiders.new_spider import NewSpider
 from scrapy.crawler import CrawlerProcess
-
+import celery_config
 
 app = Celery('tasks', broker='mongodb://localhost:27017/celery_data', backend='mongodb://localhost:27017/celery_data')
+app.config_from_object(celery_config)
 
 
-@app.tasks
-def tasks():
+@app.task
+def task1():
+    print "\n\n in task1 method ... "
     crawl()
 
 
-@app.tasks
+@app.task
 def new_task():
+    print "\n\n in new_task method"
     process = CrawlerProcess(get_project_settings())
     process.crawl(ImagesSpider)
     process.crawl(MySpider)
